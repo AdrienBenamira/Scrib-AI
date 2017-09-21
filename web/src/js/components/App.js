cc820541865f115a9642d33d071832a569105515
bdvllrs <bdvllrs@gmail.com>
2017-09-21 16:01:48 +0200

    Import Link and Route bis

--- web/src/js/components/App.js
+++ web/src/js/components/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Link, Route} from "react-router-dom";
+import Summarize from "./Summarize";
 
 export default class App extends Component {
     constructor() {
