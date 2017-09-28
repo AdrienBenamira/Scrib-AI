3fce780c19d9da5acd2c898024c8f99fd7babba5
adribenben <benamira.adrien@gmail.com>
2017-09-28 15:50:46 +0200

    add identification fonction for post

--- web/src/js/components/Login.js
+++ web/src/js/components/Login.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import Form from "./glui/form/Form";
 import Input from "./glui/form/Input";
 import {connectUser, userConnected} from "../actions/userAction";
+import Identification from "../../../../API/Identification";
+
+
 
 export const Login = (props) => {
     return (
@@ -11,10 +14,15 @@ export const Login = (props) => {
                 onSubmit={values => props.dispatch(dispatch => {
                     dispatch(connectUser(values));
                     //TODO: API CALL
+                    var name=this.ids.username.value;
+                    var password=this.ids.password.value;
+
+                    Identification.postIdentification(name,password);
+
                     dispatch(userConnected());
                 })}>
                     <Input id="username" label="Email Adress" required/>
-                    <Input id="password" label="Password" required/>
+                    <Input id="password" type="password" label="Password" required/>
                 </Form>
             </div>
         </div>
