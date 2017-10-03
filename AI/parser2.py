075ba838f69b3bffcccc1bb7e7efa204b697d19e
adribenben <benamira.adrien@gmail.com>
2017-10-04 01:21:37 +0200

    V1 parsing HTML mais on n'arrivera jamais au niveau de Google traduction...

--- AI/parser2.py
+++ AI/parser2.py
@@ -1,49 +1,75 @@
+ # -*- coding: utf-8 -*-
 import requests
 import justext
+from newspaper import Article
 import json
 import os
 import time
 import falcon
 import re
 
+
 print('Ready')
 
 def HTML_from_url(url):
     #url as string
     texte=''
-    response = requests.get(url)
-    paragraphs = justext.justext(response.content, justext.get_stoplist("English"))
-    for paragraph in paragraphs:
-      if not paragraph.is_boilerplate:
-          path=paragraph.dom_path.split('.')
-          if path[len(path)-1]=='p':
-              texte=texte+paragraph.text.encode('utf8')
+    article = Article(url, keep_article_html=True)
+    article.download()
+    article.parse()
+    texte=article.text
 
     url_resume = "http://127.0.0.1:8000/summarize_site"
     headers = {}
-    response = requests.post(url_resume, data=json.dumps({'article':str(texte)}),headers=headers)
+    response = requests.post(url_resume, data=json.dumps({'article':texte}),headers=headers)
     article_resume=json.loads(response.text)['resp_resume']
-
-    site = requests.get(url)
+    site = article.html
+    html_article=article.article_html
     compteur=0
-    paragraphs = justext.justext(site.content, justext.get_stoplist("English"))
-    for paragraph in paragraphs:
-      if not paragraph.is_boilerplate:
-          path=paragraph.dom_path.split('.')
-          if path[len(path)-1]=='p':
-              compteur=compteur+1
-              print('compteur='+str(compteur))
-              if compteur==1:
-                  print(paragraph.text)
-                  site_final=site.content.replace(paragraph.text,article_resume)
+    sentences = justext.justext(html_article, justext.get_stoplist("English"))
+    print(article_resume)
+    print ''
+    print site
+    print ''
+    L=[]
+    for sentence in sentences:
+          print('compteur='+str(compteur))
+          if compteur==0:
+              print(sentence.text)
+              print(site.find(sentence.text))
+              if site.find(sentence.text)>=0:
+                  compteur=compteur+1
               else:
-                  print(paragraph.text)
-                  site_final=site_final.replace(paragraph.text,'')
-    return(site_final)
+                  L.append(sentence.text)
+              site=site.replace(sentence.text,article_resume)
+          else:
+              print(sentence.text)
+              print(site.find(sentence.text))
+              if site.find(sentence.text)==-1:
+                  L.append(sentence.text)
+              site=site.replace(sentence.text,' ')
+    try:
+        for sentence_bis in L:
+            if len(sentence_bis.split(' '))>2:
+                sentence_bis=sentence_bis.split(' ')
+                mot1= sentence_bis[0]+' '+sentence_bis[1]
+                fin1= sentence_bis[len(sentence_bis)-2]+' '+sentence_bis[len(sentence_bis)-1]
+                print(mot1, fin1)
+                site=re.sub(mot1+'.*'+fin1,'',site, flags=re.DOTALL)
+                return(site)
+    except ValueError:
+        return(site)
+
+def get_infos_article(url):
+    article = Article(url)
+    article.download()
+    article.parse()
+    return(article.title,article.authors,article.publish_date,article.keywords)
 
 
 class LetsTransformeSite:
     def on_post(self, req,resp):
+        print("START TRANSFORM SITE")
         t0=time.time()
         compteur=0
         try:
@@ -58,9 +84,35 @@ class LetsTransformeSite:
                 'chrono': t1-t0
             }
             print(t1-t0)
+            print("END TRANSFORM SITE")
+            resp.body = json.dumps(content)
+        except IOError as e:
+            print e
+
+class Information:
+    def on_post(self, req,resp):
+        print("START get infos")
+        t01=time.time()
+        compteur=0
+        try:
+            response=req.stream.read()
+            #print(response)
+            response=json.loads(response)
+            title,authors,publish_date,keywords=get_infos_article(str(response['url']))
+            t11=time.time()
+            content={
+                'status':'sucess',
+                'titre':title,
+                'chrono': authors,
+                'publish_date':publish_date,
+                'keywords':keywords
+            }
+            print(t11-t01)
+            print("END get infos")
             resp.body = json.dumps(content)
         except IOError as e:
             print e
 
 api2=falcon.API()
 api2.add_route('/newsite',LetsTransformeSite())
+api2.add_route('/infos',Information())
--- AI/test_API_parser2.py
+++ AI/test_API_parser2.py
@@ -1,10 +1,10 @@
 import requests
 import json
-url_resume = "http://127.0.0.1:8000/newsite"
+url_resume = "http://127.0.0.1:8001/newsite"
 headers = {}
-response = requests.post(url_resume, data=json.dumps({'url':'http://www.bbc.com/news/world-us-canada-41466116'}),headers=headers)
+response = requests.post(url_resume, data=json.dumps({'url':'https://gizmodo.com/us-homeland-security-will-start-collecting-social-media-1818777094'}),headers=headers)
 print(response)
 site_resume=json.loads(response.text)['site_resume']
 
 with open("site.html", "w") as output:
-    output.write(site_resume)
+    output.write(site_resume.encode('utf8'))
