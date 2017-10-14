148e63cf88a09f72e7899b2cb5156bc6c1905403
adribenben <benamira.adrien@gmail.com>
2017-10-14 12:20:53 +0200

    fix bugs

--- AI/main.py
+++ AI/main.py
@@ -16,18 +16,18 @@ with open(config_path, 'r') as config_file:
     config = json.loads(config_file.read())
 
 
-def summarize():
+def summarize(response):
     print('START summarization')
     t0=time.time()
-    response = response.json()['payload']
-    nbre_words_input=len(response['article'].encode('utf-8').split())
+    response['payload'] = json.loads(response['payload'])
+    nbre_words_input=len(response['payload']['article'].encode('utf-8').split())
     nbre_words_input=nbre_words_input+int(nbre_words_input*0.1)
-    ratio=float(response['ratio'])
+    ratio=float(response["payload"]['ratio'])
     print(ratio)
     nbre_words_output=int(nbre_words_input*ratio)
     print(nbre_words_input,nbre_words_output)
     with open("finished_files/original.source", "w") as output:
-        output.write(str(response['article'].encode('utf-8')))
+        output.write(str(response['payload']['article'].encode('utf-8')))
     resultat=fonction_principale(nbre_words_input,nbre_words_output)
     t1=time.time()
     Nc=len(resultat.split())
@@ -48,42 +48,43 @@ def summarize():
     requests.delete(config['host'] + '/api/queue/task', json=content)
     print('FINISH summarization')
 
-    def sumarize_site(response):
-        print("START TRANSFORM SITE")
-        t0=time.time()
-        compteur=0
-        (art, titre,authors,publish_date,keywords, images)=article_from_url(str(response['url']), req, resp)
-        nbre_words_input=len(art.encode('utf-8').split())
-        nbre_words_input=nbre_words_input+int(nbre_words_input*0.1)
-        ratio=0.30
-        nbre_words_output=int(nbre_words_input*ratio)
-        print(nbre_words_input,nbre_words_output)
-        with open("finished_files/original.source", "w") as output:
-            output.write(str(art.encode('utf-8')))
-        resultat=fonction_principale(nbre_words_input,nbre_words_output)
-        t1=time.time()
-        Nc=len(resultat.split())
-        gain=(60*(nbre_words_input-Nc)/300)-(t1-t0)
-        content={
-            'id': response['id'],
-            'uid': response['uid'],
-            'type': 'url',
-            'response': {
-                'status':'sucess',
-                'resp_resume':resultat,
-                'texte_original':art,
-                'titre':titre,
-                'authors':authors,
-                'publish_date':publish_date,
-                'keywords':keywords,
-                'images':images,
-                'chrono': t1-t0
-            }
+def sumarize_site(response):
+    print("START TRANSFORM SITE")
+    response['payload'] = json.loads(response['payload'])
+    t0=time.time()
+    compteur=0
+    (art, titre,authors,publish_date,keywords, images)=article_from_url(str(response['payload']['url']), req, resp)
+    nbre_words_input=len(art.encode('utf-8').split())
+    nbre_words_input=nbre_words_input+int(nbre_words_input*0.1)
+    ratio=0.30
+    nbre_words_output=int(nbre_words_input*ratio)
+    print(nbre_words_input,nbre_words_output)
+    with open("finished_files/original.source", "w") as output:
+        output.write(str(art.encode('utf-8')))
+    resultat=fonction_principale(nbre_words_input,nbre_words_output)
+    t1=time.time()
+    Nc=len(resultat.split())
+    gain=(60*(nbre_words_input-Nc)/300)-(t1-t0)
+    content={
+        'id': response['id'],
+        'uid': response['uid'],
+        'type': 'url',
+        'response': {
+    	'status':'sucess',
+    	'resp_resume':resultat,
+    	'texte_original':art,
+    	'titre':titre,
+    	'authors':authors,
+    	'publish_date':publish_date,
+    	'keywords':keywords,
+    	'images':images,
+    	'chrono': t1-t0
         }
-        print(t1-t0, gain)
-        requests.delete(config['host'] + '/api/queue/task', json=content)
-        print("END TRANSFORM SITE")
-        
+    }
+    print(t1-t0, gain)
+    requests.delete(config['host'] + '/api/queue/task', json=content)
+    print("END TRANSFORM SITE")
+
 def main():
     print('Get last file in queue')
     response = requests.get(config['host'] + '/api/queue/task')
